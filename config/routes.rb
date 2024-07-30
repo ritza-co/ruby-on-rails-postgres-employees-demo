47ee1d5b807e02d57e9ff441273d2a3eaf553673
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-07-30 11:03:23 +0200

    Routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'employees/index'
-  get 'employees/create'
-  get 'employees/update'
-  get 'employees/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'employees#index'
+  resources :employees, only: [:index, :create, :update, :destroy]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Keep the health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
