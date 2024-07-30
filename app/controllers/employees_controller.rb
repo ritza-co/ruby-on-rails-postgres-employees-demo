d5ac13eb4e8a88d7f8391b4f002239038f3e9430
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-07-30 11:01:44 +0200

    Create Employees controller

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,13 @@
+class EmployeesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+  get 'employees/create'
+  get 'employees/update'
+  get 'employees/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
